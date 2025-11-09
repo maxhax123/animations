c4c1706ea99123554ed55bb57d1212e4c6f34a3a
maxhax123 <127565011+maxhax123@users.noreply.github.com>
2025-11-08 19:40:46 -1000

    Add files via upload

---
+++ fe pack.lua
@@ -0,0 +1,5 @@
+﻿loadstring(game:HttpGet("https://pastefy.app/YsJgITXR/raw"))() --fe r6 096
+loadstring(game:HttpGet("https://pastefy.app/JOWniO6o/raw"))() --fe zombie boy
+loadstring(game:HttpGet("https://pastefy.app/wxVAgZpT/raw"))() --fighter
+loadstring(game:HttpGet("https://pastefy.app/riYch0rX/raw"))() --killer
+loadstring(game:HttpGet("https://pastefy.app/YJITpCRA/raw"))() --psyco
